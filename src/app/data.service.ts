16e8b40eee593249f7b8901470b2a0bb80913f65
khan shamsiya sehroz <104444008+shamsiyakhan@users.noreply.github.com>
2022-04-28 15:06:01 +0530

    Update data.service.ts

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -9,21 +9,21 @@ export class DataService {
   constructor(private http:HttpClient) { }
   getcurrentdata(a1:any){
 
-          let url="https://api.openweathermap.org/data/2.5/weather?q="+a1+"&appid=984fd452896fcdf356d9c08f1de6e3c7" 
+          let url="https://api.openweathermap.org/data/2.5/weather?q="+a1+"&appid=" 
 
           return this.http.get(url)    
   }
   daysweather(d:any){
 
 
-          let url1="https://api.openweathermap.org/data/2.5/forecast?q="+d+"&appid=511804e092b9f9869dfb4497d48ec8b1"
+          let url1="https://api.openweathermap.org/data/2.5/forecast?q="+d+"&appid="
           
             return this.http.get(url1)
   }
 
   airquality(lat:any, long:any){
 
-        let url="https://api.openweathermap.org/data/2.5/air_pollution?lat="+lat+"&lon="+long+"&appid=511804e092b9f9869dfb4497d48ec8b1"
+        let url="https://api.openweathermap.org/data/2.5/air_pollution?lat="+lat+"&lon="+long+"&appid="
             return this.http.get(url)
 
   }
